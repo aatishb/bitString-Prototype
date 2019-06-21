f60ccfbfa12510bf53eeea976ce03c02532100af
Aatish Bhatia <aatish@gmail.com>
2019-06-21 15:05:04 -0400

    some changes

--- p5-sketch1.js
+++ p5-sketch1.js
@@ -3,24 +3,35 @@
 
 function sketch(parent) { // we pass the sketch data from the parent
     return function( p ) {
-        
+
       // create new osc
-      var osc = new p5.Oscillator();
-      
+      let osc;
+      let amp = 0.5, freq = 440;
+
       p.setup = function() {
         let canvas = p.createCanvas(400, 400);
         canvas.parent(parent.$el); // links child to parent
+        p.background(p.map(freq, 200, 2000, 0, 255)); // color changes based on freq
+
+        osc = new p5.Oscillator();
         osc.setType('sine');
-        osc.freq(400);
-        osc.amp(0);
+        osc.freq(freq);
+        osc.amp(amp);
         osc.start();
-  
       };
-  
+
       p.draw = function() {
-        p.background(p.map(parent.data.baseFreq, 200, 2000, 0, 255)); // color changes based on freq
-        osc.amp(parent.data.amp);
-        osc.freq(parent.data.baseFreq);
+        if (amp !== parent.data.amp || freq !== parent.data.baseFreq){
+          amp = parent.data.amp;
+          freq = parent.data.baseFreq;
+          p.background(p.map(freq, 200, 2000, 0, 255)); // color changes based on freq
+          console.log(amp);
+          console.log(freq);
+          osc.amp(amp);
+          osc.freq(freq);
+
+        }
+
       };
     };
   }
